e2324b59ffe788d4b6b9ccc9c5dc700894f04b86
zyte09 <allen.aranez32@gmail.com>
2024-11-05 17:12:32 +0800

    Add project files.

---
+++ CSProject1/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        Label4 = New Label()
+        Button1 = New Button()
+        Output = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Honor = New Label()
+        SuspendLayout()
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.ImageScalingSize = New Size(20, 20)
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(61, 4)
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(90, 31)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(114, 35)
+        Label1.TabIndex = 1
+        Label1.Text = "Subjects"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 10F)
+        Label2.Location = New Point(90, 79)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(78, 23)
+        Label2.TabIndex = 1
+        Label2.Text = "CSAR101"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 10F)
+        Label3.Location = New Point(90, 123)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(73, 23)
+        Label3.TabIndex = 1
+        Label3.Text = "ITECC05"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 10F)
+        Label7.Location = New Point(90, 165)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(77, 23)
+        Label7.TabIndex = 1
+        Label7.Text = "CSSDF01"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 10F)
+        Label8.Location = New Point(90, 212)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(86, 23)
+        Label8.TabIndex = 1
+        Label8.Text = "CALPH++"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 10F)
+        Label9.Location = New Point(90, 258)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(75, 23)
+        Label9.TabIndex = 1
+        Label9.Text = "LINALGE"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BorderStyle = BorderStyle.FixedSingle
+        TextBox1.Location = New Point(223, 79)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BorderStyle = BorderStyle.FixedSingle
+        TextBox2.Location = New Point(223, 123)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 3
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.BorderStyle = BorderStyle.FixedSingle
+        TextBox3.Location = New Point(223, 165)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 4
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.BorderStyle = BorderStyle.FixedSingle
+        TextBox4.Location = New Point(223, 212)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 27)
+        TextBox4.TabIndex = 5
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.BorderStyle = BorderStyle.FixedSingle
+        TextBox5.Location = New Point(223, 258)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(125, 27)
+        TextBox5.TabIndex = 6
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(223, 31)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(96, 35)
+        Label4.TabIndex = 1
+        Label4.Text = "Grades"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 15F)
+        Button1.Location = New Point(411, 172)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(137, 54)
+        Button1.TabIndex = 7
+        Button1.Text = "Calculate"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Output
+        ' 
+        Output.AutoSize = True
+        Output.Location = New Point(494, 81)
+        Output.Name = "Output"
+        Output.Size = New Size(17, 20)
+        Output.TabIndex = 8
+        Output.Text = "0"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 10F)
+        Label5.Location = New Point(411, 79)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(60, 23)
+        Label5.TabIndex = 1
+        Label5.Text = "Result:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(411, 126)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(54, 20)
+        Label6.TabIndex = 8
+        Label6.Text = "Honor:"
+        ' 
+        ' Honor
+        ' 
+        Honor.AutoSize = True
+        Honor.Location = New Point(494, 130)
+        Honor.Name = "Honor"
+        Honor.Size = New Size(36, 20)
+        Honor.TabIndex = 9
+        Honor.Text = "........."
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Honor)
+        Controls.Add(Label6)
+        Controls.Add(Output)
+        Controls.Add(Button1)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label3)
+        Controls.Add(Label5)
+        Controls.Add(Label2)
+        Controls.Add(Label4)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Output As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Honor As Label
+
+End Class
---
+++ CSProject1/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1, num2, num3, num4, num5 As Integer
+        num1 = TextBox1.Text
+        num2 = TextBox2.Text
+        num3 = TextBox3.Text
+        num4 = TextBox4.Text
+        num5 = TextBox5.Text
+
+        Dim grades As New List(Of Integer) From {num1, num2, num3, num4, num5}
+        Dim average As Double = grades.Average()
+
+        Output.Text = average
+
+        If grades.All(Function(g) g >= 85) AndAlso average > 90 Then
+            Honor.Text = "Dean's Lister"
+        ElseIf grades.All(Function(g) g >= 95) AndAlso average >= 90 Then
+            Honor.Text = "President's Lister"
+        Else
+            Honor.Text = "No Honor"
+        End If
+
+    End Sub
+
+End Class
